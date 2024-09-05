cacaa50280dbffeb5a7ec273c4bebc5c3733989b
Mehrnaz <mehrnazhemmat1998@gmail.com>
2024-09-06 00:37:24 +0330

    S06-E03: How to NOT fetch data in React.js?

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,16 @@ import { allcharacters } from "../data/data";
 import { useState } from "react";
 
 export default function App() {
-  const [characters, setCharacters] = useState(allcharacters);
+  const [characters, setCharacters] = useState([]);
+
+  // NOT to fetch in this way: We shouldn't write fetch in render logic
+  fetch("https://rickandmortyapi.com/api/character")
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+
   return (
     <div className="app">
-      <Navbar numOfResult={characters.length}>
+      <Navbar>
         <SearchResult numOfResult={characters.length} />
       </Navbar>
       <Main characters={characters}>
